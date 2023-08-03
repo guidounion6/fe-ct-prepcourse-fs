7221ad409fa6f1acae58e7be42bbde90ccf10313
guido_union6 <guido_union6@hotmail.com>
2023-08-03 10:47:01 -0300

    Ejercicios M3

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -3,7 +3,8 @@
 
 function devolverString(string) {
    // Debe retornar un string.
-   // Tu código:
+   // Tu código: 
+   return string;
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +12,36 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+  return x + y 
+  suma(5,6);
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   return x - y 
+   resta(5,6); 
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   return x / y 
+   divide(9,3);
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   return x * y
+   multiplica(5,8);
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   return x % y 
+   obtenerResto(20,4);
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
